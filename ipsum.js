b5cd3368554c7da2cf731eadca0921a9a799fd47
Brian Dukes <bdukes@engagesoftware.com>
2011-06-10 09:51:15 -0500

    Added jade view engine

--- ipsum.js
+++ ipsum.js
@@ -2,9 +2,10 @@ var express = require('express');
 var data = require('./data.js');
 
 var app = express.createServer(express.logger());
+app.set('view engine', 'jade');
 
 app.get('/', function (request, response) {
-	response.send('<h1>Hello world from Star Wars Ipsum</h1><p>' + data.terms[0] + '</p>');
+	response.render('index');
 });
 
 var port = process.env.PORT || 3000;
