878558a72d26472f45d1408a1b33a50913a94c55
Brian Dukes <dukes.brian@gmail.com>
2011-07-15 23:22:59 -0500

    Added disclaimer page and footer nav.  Closes #5

--- ipsum.js
+++ ipsum.js
@@ -9,6 +9,9 @@ app.set('view engine', 'jade');
 app.get('/', function (request, response) {
 	response.render('index', {paragraphs:[]});
 });
+app.get('/dislaimer', function (request, response) {
+	response.render('disclaimer');
+});
 app.post('/', function (request, response) {
 	var paragraphCount = 5,
 	    paragraphLength = 500,
